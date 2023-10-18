dbb7689f2aac180e4f4908856c7828f766bf663c
Karlos Warney <karloswarney@gmail.com>
2023-10-18 18:48:47 -0300

    ADD - Ads vertical adicionado, agora basta inserir a prop :vertical="true" para carregar o anúncio vertical.

--- src/components/globalComponents/AdsComponent.vue
+++ src/components/globalComponents/AdsComponent.vue
@@ -1,15 +1,21 @@
 <template>
-  <iframe v-if="!isMobile && isActive"
+  <iframe v-if="!isMobile && isActive && !$props.vertical"
           style="border: none; background-color: #212121"
           width="728px"
           height="90px"
           :src="$urlBase + '/api/ads/banner-728x90'"
           sandbox="allow-scripts allow-same-origin allow-popups"></iframe>
-  <iframe v-if="isMobile && isActive"
+  <iframe v-if="isMobile && isActive && !$props.vertical"
           style="border: none; background-color: #212121"
           width="320px"
           height="50px"
           :src="$urlBase + '/api/ads/banner-320x50'"
+          sandbox="allow-scripts allow-same-origin allow-popups"></iframe>
+  <iframe v-if="!isMobile && isActive && $props.vertical"
+          style="border: none; background-color: #212121"
+          width="300px"
+          height="250px"
+          :src="$urlBase + '/api/ads/banner-300x250'"
           sandbox="allow-scripts allow-same-origin allow-popups"></iframe>
 </template>
 
@@ -23,10 +29,18 @@ export default {
     isActive(){
       if(this.$store.main.isLogged){
         let rank = this.$store.user.getRanking;
-        return this.$store.main.getAdsActive && rank < 5;
+        return this.$store.main.getAdsActive && rank > 5;
       }
       return this.$store.main.getAdsActive;
     }
+  },
+  props: {
+    vertical: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted() {
   }
 };
 </script>
